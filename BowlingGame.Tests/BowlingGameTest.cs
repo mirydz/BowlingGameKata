be9e257c920151889195761b474fdce770c45b81
Michal Rydz <mirydz@gmail.com>
2015-09-09 00:21:38 +0200

    test 2

--- BowlingGame.Tests/BowlingGameTest.cs
+++ BowlingGame.Tests/BowlingGameTest.cs
@@ -11,6 +11,20 @@ namespace BowlingGame.Tests
     [TestFixture]
     public class BowlingGameTest
     {
+        private Game game;
+
+        [SetUp]
+        public void Init()
+        {
+            game = new Game();
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for (var i = 0; i < n; i++)
+                game.Roll(pins);
+        }
+
         [Test]
         public void ItCompiles()
         {
@@ -20,13 +34,17 @@ namespace BowlingGame.Tests
         [Test]
         public void TestGutterGame()
         {
-            var game = new Game();
-
-            for (var i = 0; i < 20; i++)
-                game.Roll(0);
+            RollMany(20, 0);
 
             Assert.AreEqual(0, game.Score());
+        }
 
+        [Test]
+        public void TestAllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.AreEqual(20, game.Score());
         }
     }
 }
