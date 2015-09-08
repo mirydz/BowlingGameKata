1dfec3729ff5d56f900494183b491585beda5512
Michal Rydz <mirydz@gmail.com>
2015-09-08 23:02:07 +0200

    zero test

---
+++ BowlingGame.Tests/BowlingGameTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Core;
+using NUnit.Framework;
+
+namespace BowlingGame.Tests
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        [Test]
+        public void ItCompiles()
+        {
+            Assert.True(true);
+        }
+    }
+}
